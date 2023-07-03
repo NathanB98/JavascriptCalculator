09c20cb8ebb81923b0ad8f01b84947c69925c338
NathanB98 <97767277+NathanB98@users.noreply.github.com>
2023-07-03 21:44:44 +0100

    Creating querySelectors for buttons.

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const NUMBER_BUTTONS = document.querySelectorAll('[data-number]');
+const OPERATOR_BUTTONS = document.querySelectorAll('[data-operator]');
+const DECIMAL_BUTTON = document.querySelector('.btn-decimal');
+const EQUALS_BUTTON = document.querySelector('.btn-equal');
+const CLEAR_BUTTON = document.querySelector('.btn-clear');
